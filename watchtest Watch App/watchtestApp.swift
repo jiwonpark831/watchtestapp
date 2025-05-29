dff9062b9dfba7e7b2f429f1143a9e700f63cd66
jiwonpark831 <jiwonpark831@gmail.com>
2025-05-30 01:11:56 +0900

    make repo

---
+++ watchtest Watch App/watchtestApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchtestApp.swift
+//  watchtest Watch App
+//
+//  Created by jiwon on 5/30/25.
+//
+
+import SwiftUI
+
+@main
+struct watchtest_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
